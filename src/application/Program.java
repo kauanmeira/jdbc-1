832b3d944933e21360b0e2c9cdc89fb5c679a1ac
kauanmeira <kauangabriel.meira123@gmail.com>
2023-03-22 12:44:53 -0300

    Inserting data into database

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,6 +18,7 @@ public class Program {
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
+			/*
 			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, "Kauan Meira");
@@ -25,7 +26,8 @@ public class Program {
 			st.setDate(3, new java.sql.Date(sdf.parse("19/04/2002").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
-
+*/
+		st = conn.prepareStatement ("insert into department(Name) values ('D1'), ('D2')", Statement.RETURN_GENERATED_KEYS);
 			int rowsAffected = st.executeUpdate();
 			if (rowsAffected >0) {
 				ResultSet rs = st.getGeneratedKeys();
@@ -39,8 +41,7 @@ public class Program {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
+		
 		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
